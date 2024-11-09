e842a42583d6fffadfa97525612214062b23c71f
Captain Sherman <cptn.sherman@gmail.com>
2024-11-09 17:57:25 -0500

    define screenshot keycode

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use bevy::render::texture::{
     ImageAddressMode, ImageLoaderSettings, ImageSampler, ImageSamplerDescriptor,
 };
 use bevy::render::view::screenshot::ScreenshotManager;
-use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy::window::PrimaryWindow;
 use bevy::{
     core_pipeline::{
         experimental::taa::{TemporalAntiAliasBundle, TemporalAntiAliasPlugin},
@@ -49,7 +49,7 @@ use bevy_dev_console::prelude::*;
 use transvoxel::{transition_sides, voxel_source::Block};
 use user_interface::DebugInterfacePlugin;
 use utils::{
-    detect_toggle_cursor, format_value_f32, get_valid_extension, increase_render_adapter_wgpu_limits, CHUNK_SIZE_F32
+    detect_toggle_cursor, format_value_f32, generate_plane_mesh, get_valid_extension, increase_render_adapter_wgpu_limits, CHUNK_SIZE_F32
 };
 
 #[derive(Component)]
@@ -67,6 +67,7 @@ pub struct KeyBindings {
     pub toggle_sprint: KeyCode,
     pub toggle_grab_cursor: KeyCode,
     pub interact: KeyCode,
+    pub take_screenshot: KeyCode,
 }
 
 impl Default for KeyBindings {
@@ -81,6 +82,7 @@ impl Default for KeyBindings {
             toggle_sprint: KeyCode::ShiftLeft,
             toggle_grab_cursor: KeyCode::Escape,
             interact: KeyCode::KeyE,
+            take_screenshot: KeyCode::Equal,
         }
     }
 }
@@ -123,7 +125,7 @@ fn main() {
             (
                 animate_light_direction,
                 detect_toggle_cursor,
-                screenshot_on_equals,
+                take_screenshot,
             ),
         )
         .run();
@@ -358,83 +360,17 @@ fn setup(
     });
 }
 
-fn generate_plane_mesh(
-    meshes: &mut ResMut<Assets<Mesh>>,
-    width: f32,
-    length: f32,
-    uv_scale: f32,
-) -> Handle<Mesh> {
-    let half_width = width / 2.0;
-    let half_length = length / 2.0;
-
-    let vertices = vec![
-        // Top face
-        (
-            [-half_width, 0.0, half_length],
-            [0.0, 1.0, 0.0],
-            [0.0, uv_scale * length],
-        ), // Top-left
-        (
-            [half_width, 0.0, half_length],
-            [0.0, 1.0, 0.0],
-            [uv_scale * width, uv_scale * length],
-        ), // Top-right
-        (
-            [half_width, 0.0, -half_length],
-            [0.0, 1.0, 0.0],
-            [uv_scale * width, 0.0],
-        ), // Bottom-right
-        (
-            [-half_width, 0.0, -half_length],
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0],
-        ), // Bottom-left
-    ];
-
-    let indices = vec![
-        0, 1, 2, 2, 3, 0, // top face
-    ];
-
-    let mut positions = Vec::new();
-    let mut normals = Vec::new();
-    let mut uvs = Vec::new();
-
-    for (position, normal, uv) in vertices {
-        positions.push(position);
-        normals.push(normal);
-        uvs.push(uv);
-    }
-
-    let mut mesh = Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::RENDER_WORLD,
-    );
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        VertexAttributeValues::from(positions),
-    );
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, VertexAttributeValues::from(normals));
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::from(uvs));
-    mesh.insert_indices(Indices::U32(indices));
-
-    meshes.add(
-        mesh.with_generated_tangents()
-            .expect("Failed to generate tangents for the mesh"),
-    )
-}
 
 
-
-
-
-/** This system was yonked from the screenshot example: https://bevyengine.org/examples/Window/screenshot/ */
-fn screenshot_on_equals(
+/** This system was taken from the screenshot example: https://bevyengine.org/examples/Window/screenshot/ */
+fn take_screenshot(
     settings: Res<EngineSettings>,
+    key_bindings: Res<KeyBindings>,
     keys: Res<ButtonInput<KeyCode>>,
     main_window: Query<Entity, With<PrimaryWindow>>,
     mut screenshot_manager: ResMut<ScreenshotManager>,
 ) {
-    if keys.just_pressed(KeyCode::Equal) {
+    if keys.just_pressed(key_bindings.take_screenshot) {
         // get the formated path as string.
         let date: DateTime<Local> = Local::now();
         let formated_date: chrono::format::DelayedFormat<chrono::format::StrftimeItems> =
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -80,7 +80,7 @@ macro_rules! double_for_loop {
 }
 
 use bevy::{
-    input::ButtonInput, log::{info, warn}, math::Vec2, prelude::{Image, KeyCode, Query, Res, With}, render::{render_asset::RenderAssetUsages, render_resource::{Extent3d, TextureDimension, TextureFormat}, renderer::RenderAdapter}, window::{CursorGrabMode, PrimaryWindow, Window}
+    asset::{Assets, Handle}, input::ButtonInput, log::{info, warn}, math::Vec2, prelude::{Image, KeyCode, Mesh, Query, Res, ResMut, With}, render::{mesh::{Indices, PrimitiveTopology, VertexAttributeValues}, render_asset::RenderAssetUsages, render_resource::{Extent3d, TextureDimension, TextureFormat}, renderer::RenderAdapter}, window::{CursorGrabMode, PrimaryWindow, Window}
 };
 use std::fmt::Write;
 
@@ -251,6 +251,71 @@ pub fn uv_debug_texture() -> Image {
 
     // Return the generated image
     img
+}
+
+pub fn generate_plane_mesh(
+    meshes: &mut ResMut<Assets<Mesh>>,
+    width: f32,
+    length: f32,
+    uv_scale: f32,
+) -> Handle<Mesh> {
+    let half_width = width / 2.0;
+    let half_length = length / 2.0;
+
+    let vertices = vec![
+        // Top face
+        (
+            [-half_width, 0.0, half_length],
+            [0.0, 1.0, 0.0],
+            [0.0, uv_scale * length],
+        ), // Top-left
+        (
+            [half_width, 0.0, half_length],
+            [0.0, 1.0, 0.0],
+            [uv_scale * width, uv_scale * length],
+        ), // Top-right
+        (
+            [half_width, 0.0, -half_length],
+            [0.0, 1.0, 0.0],
+            [uv_scale * width, 0.0],
+        ), // Bottom-right
+        (
+            [-half_width, 0.0, -half_length],
+            [0.0, 1.0, 0.0],
+            [0.0, 0.0],
+        ), // Bottom-left
+    ];
+
+    let indices = vec![
+        0, 1, 2, 2, 3, 0, // top face
+    ];
+
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+
+    for (position, normal, uv) in vertices {
+        positions.push(position);
+        normals.push(normal);
+        uvs.push(uv);
+    }
+
+    let mut mesh = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        VertexAttributeValues::from(positions),
+    );
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, VertexAttributeValues::from(normals));
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::from(uvs));
+    mesh.insert_indices(Indices::U32(indices));
+
+    meshes.add(
+        mesh.with_generated_tangents()
+            .expect("Failed to generate tangents for the mesh"),
+    )
 }
 
 // --- Validating file extensions ---
