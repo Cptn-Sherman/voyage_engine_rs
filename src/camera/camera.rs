3f9d360ef8badc9bfa8f489cb73b6fa46c8d587d
Captain Sherman <cptn.sherman@gmail.com>
2025-01-11 20:25:17 -0500

    the latest and greatest

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -4,8 +4,7 @@ use bevy::{
     },
     math::Vec3,
     pbr::{ScreenSpaceAmbientOcclusion, ScreenSpaceReflections, VolumetricFog},
-    prelude::{Camera, Camera3d, ClearColorConfig, Commands, Res, Transform},
-    ui::UiAntiAlias,
+    prelude::*,
     utils::default,
 };
 
@@ -14,47 +13,69 @@ use crate::CameraThing;
 use super::config::CameraConfig;
 
 pub fn create_camera(mut commands: Commands, camera_config: Res<CameraConfig>) {
-    // ******************************************
-    // ** This is the main 3D camera that will render the 3D world.
-    // ** It has a few extra components to enable some post-processing
-    // ** effects like volumetric fog, screen space ambient occlusion, and temporal anti-aliasing.
-    // ** It also has a blur regions camera component to enable the blur regions post-processing effect.
-    // ******************************************
-
     commands
         .spawn((
             Camera3d::default(),
             Camera {
+                order: 0,
                 hdr: camera_config.hdr,
                 ..default()
             },
+            Transform::from_xyz(0.0, 0.0, 0.0).looking_to(Vec3::ZERO, Vec3::Y),
+            Tonemapping::ReinhardLuminance,
             TemporalAntiAliasing { ..default() },
             ScreenSpaceAmbientOcclusion { ..default() },
             ScreenSpaceReflections { ..default() },
             MotionBlur { ..default() },
-            Transform::from_xyz(0.0, 0.0, 0.0).looking_to(Vec3::ZERO, Vec3::Y),
-            Tonemapping::TonyMcMapface,
             CameraThing,
         ))
         .insert(VolumetricFog {
             ambient_intensity: 0.0,
             ..default()
         });
+}
 
-    // ******************************************
-    // ** This is the 2D camera that will render the 2D UI on top of the 3D world.
-    // ** It has an order of 1, so it will render on top of the 3D camera.
-    // ** It has no clear color, so it will render on top of the 3D camera without clearing the screen.
-    // ** It has anti-aliasing enabled, so the UI will look nice and smooth.
-    // ******************************************
-
+pub fn create_fly_camera(mut commands: Commands) {
     commands.spawn((
-        Camera {
-            order: 1,
-            hdr: camera_config.hdr,
-            clear_color: ClearColorConfig::None,
-            ..default()
-        },
-        UiAntiAlias::On,
+        Transform::from_xyz(0.0, 0.0, 0.0).looking_to(Vec3::ZERO, Vec3::Y),
+        FlyCamera,
     ));
 }
+
+#[derive(Component)]
+pub struct FlyCamera;
+
+// pub fn move_fly_camera(mut query: Query<(&mut Transform, With<FlyCamera>)>, time: Res<Time>) {
+//     for (mut transform,) in query.iter_mut() {
+//         let mut translation = transform.translation;
+//         let mut rotation = transform.rotation;
+
+//         let speed = 10.0;
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::KeyW) {
+//             translation += rotation.mul_vec3(Vec3::Z) * speed * time.delta_seconds();
+//         }
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::KeyS) {
+//             translation -= rotation.mul_vec3(Vec3::Z) * speed * time.delta_seconds();
+//         }
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::KeyA) {
+//             translation -= rotation.mul_vec3(Vec3::X) * speed * time.delta_seconds();
+//         }
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::KeyD) {
+//             translation += rotation.mul_vec3(Vec3::X) * speed * time.delta_seconds();
+//         }
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::Space) {
+//             translation += Vec3::Y * speed * time.delta_seconds();
+//         }
+
+//         if bevy::input::keyboard::is_key_pressed(&bevy::input::keyboard::KeyCode::ShiftLeft) {
+//             translation -= Vec3::Y * speed * time.delta_seconds();
+//         }
+
+//         transform.translation = translation;
+//     }
+// }
--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -15,7 +15,6 @@ use crate::{
 pub mod config;
 // Todo: at some point we should have the camera load its config from a toml file or generate the default, possibly using some perfromance calculator to determine what works best for the system in use.
 pub mod camera;
-pub mod flycam;
 
 /** This system was taken from the screenshot example: https://bevyengine.org/examples/Window/screenshot/ */
 pub fn take_screenshot(
