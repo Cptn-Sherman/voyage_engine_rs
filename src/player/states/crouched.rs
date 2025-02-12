60326df6c7ee57751ae4eea45cce650c5f72480b
Captain Sherman <cptn.sherman@gmail.com>
2025-02-11 23:09:48 -0500

    add crouched key binding

--- src/player/states/crouched.rs
+++ src/player/states/crouched.rs
@@ -1,17 +1,17 @@
 use avian3d::collision::Collider;
 use bevy::prelude::*;
 
-use crate::player::{body::Body, config::PlayerControlConfig, motion::Motion, stance::Stance, Player};
+use crate::{config::KeyBindings, player::{body::Body, config::PlayerControlConfig, motion::Motion, stance::Stance, Player}};
 
 pub fn toggle_crouching(
     keys: Res<ButtonInput<KeyCode>>,
+    key_bindings: Res<KeyBindings>,
     player_config: ResMut<PlayerControlConfig>,
     mut query: Query<(&mut Body, &mut Stance, &mut Motion, &mut Collider), With<Player>>,
 ) {
     for (mut body, mut stance, mut motion, mut collider) in query.iter_mut() {
-        // Check if the control key is pressed
-        // todo: replace with a crouch key binding.
-        if !keys.just_pressed(KeyCode::ControlLeft) {
+
+        if !keys.just_pressed(key_bindings.toggle_crouched) {
             return;
         }
 
@@ -30,7 +30,6 @@ pub fn toggle_crouching(
             motion.current_movement_speed = player_config.movement_speed / 2.0;
         } else {
             collider.set_scale(Vec3::from([1.0, player_config.capsule_height, 1.0]), 4);
-            
             body.current_body_height = player_config.capsule_height;
             
             motion.target_ride_height = player_config.ride_height;
