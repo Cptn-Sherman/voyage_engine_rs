e936a75aaf108ade5e249a6a8d5b582f4c352396
Captain Sherman <cptn.sherman@gmail.com>
2025-03-01 14:00:02 -0500

    split mouse move into camera and player

--- src/player/focus.rs
+++ src/player/focus.rs
@@ -4,7 +4,7 @@ use bevy::{
     window::{CursorGrabMode, PrimaryWindow},
 };
 
-use super::config::PlayerControlConfig;
+use super::{config::PlayerControlConfig, Player};
 
 #[derive(Component)]
 pub struct Focus {
@@ -15,49 +15,56 @@ pub struct Focus {
 
 // This function and many of its helpers are ripped from, bevy_fly_cam.
 pub fn camera_look_system(
-    mut camera_query: Query<&mut Transform, With<Camera3d>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<Player>)>,
     accumulated_mouse_motion: ResMut<AccumulatedMouseMotion>,
     gamepads: Query<(Entity, &Gamepad)>,
     primary_window: Query<&Window, With<PrimaryWindow>>,
     config: Res<PlayerControlConfig>,
 ) {
     if let Ok(window) = primary_window.get_single() {
-        for mut transform in camera_query.iter_mut() {
-            let window_scale = window.height().min(window.width());
-
-            let (mut yaw, mut pitch, _) = transform.rotation.to_euler(EulerRot::YXZ);
-            match window.cursor_options.grab_mode {
-                CursorGrabMode::None => (),
-                _ => {
-                    let window_scale = window.height().min(window.width());
-                    pitch -=
-                        (config.mouse_look_sensitivity * accumulated_mouse_motion.delta.y * window_scale)
+        for mut player_transform in player_query.iter_mut() {
+            for mut cam_transform in camera_query.iter_mut() {
+                let window_scale = window.height().min(window.width());
+                let (mut yaw, _, _) = player_transform.rotation.to_euler(EulerRot::YXZ);
+                let ( _, mut pitch, _) = cam_transform.rotation.to_euler(EulerRot::YXZ);
+                match window.cursor_options.grab_mode {
+                    CursorGrabMode::None => (),
+                    _ => {
+                        let window_scale = window.height().min(window.width());
+                        pitch -= (config.mouse_look_sensitivity
+                            * accumulated_mouse_motion.delta.y
+                            * window_scale)
                             .to_radians();
-                    yaw -=
-                        (config.mouse_look_sensitivity * accumulated_mouse_motion.delta.x * window_scale)
+                        yaw -= (config.mouse_look_sensitivity
+                            * accumulated_mouse_motion.delta.x
+                            * window_scale)
                             .to_radians();
-                    pitch = pitch.clamp(-1.54, 1.54);
+                    }
                 }
+
+                if let Ok((_entity, gamepad)) = gamepads.get_single() {
+                    let right_stick_x: f32 =
+                        gamepad.get(GamepadAxis::RightStickX).unwrap_or_default();
+                    let right_stick_y: f32 =
+                        gamepad.get(GamepadAxis::RightStickY).unwrap_or_default();
+
+                    if right_stick_x.abs() > 0.1 {
+                        yaw -= (config.gamepad_look_sensitivity * right_stick_x * window_scale)
+                            .to_radians();
+                    }
+
+                    if right_stick_y.abs() > 0.1 {
+                        pitch += (config.gamepad_look_sensitivity * right_stick_y * window_scale)
+                            .to_radians();
+                    }
+                }
+                // prevent the camera from looping over itself in pitch only.
+                pitch = pitch.clamp(-1.54, 1.54);
+                // Order is important to prevent unintended roll
+                cam_transform.rotation = Quat::from_axis_angle(Vec3::X, pitch);
+                player_transform.rotation = Quat::from_axis_angle(Vec3::Y, yaw);
             }
-
-            if let Ok((_entity, gamepad)) = gamepads.get_single() {
-                let right_stick_x: f32 = gamepad.get(GamepadAxis::RightStickX).unwrap_or_default();
-                let right_stick_y: f32 = gamepad.get(GamepadAxis::RightStickY).unwrap_or_default();
-
-                if right_stick_x.abs() > 0.1 {
-                    yaw -= (config.gamepad_look_sensitivity * right_stick_x * window_scale).to_radians();
-                }
-
-                if right_stick_y.abs() > 0.1 {
-                    pitch += (config.gamepad_look_sensitivity * right_stick_y * window_scale).to_radians();
-                }
-            }
-
-            // prevent the camera from looping over itself in pitch only.
-            pitch = pitch.clamp(-1.54, 1.54);
-            // Order is important to prevent unintended roll
-            transform.rotation =
-                Quat::from_axis_angle(Vec3::Y, yaw) * Quat::from_axis_angle(Vec3::X, pitch);
         }
     }
 }
