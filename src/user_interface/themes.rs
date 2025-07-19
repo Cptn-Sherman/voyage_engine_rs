126d7b830a92540dd0bc672e057847774a72ce63
Captain Sherman <cptnSherman@gmail.com>
2025-07-19 12:34:02 -0400

    allow dead code

--- src/user_interface/themes.rs
+++ src/user_interface/themes.rs
@@ -5,14 +5,18 @@ pub const DEFAULT_DEBUG_FONT_PATH: &str = "fonts/mononoki-Bold.ttf";
 pub const DEFAULT_FONT_SIZE: f32 = 14.0;
 pub const NO_PERCENTAGE: &str = "---.-%";
 
+#[allow(dead_code)]
 pub const ORANGE_TEXT_COLOR: Color = Color::hsv(0.34, 1.0, 0.5);
+#[allow(dead_code)]
 pub const YELLOW_GREEN_TEXT_COLOR: Color = Color::hsv(0.9, 0.69, 0.58);
+#[allow(dead_code)]
 pub const RED_TEXT_COLOR: Color = Color::srgb(1.0, 0.0, 0.0);
+#[allow(dead_code)]
 pub const GOLD_TEXT_COLOR: Color = Color::srgb(1.0 , 0.72, 0.0);
 pub const BORDER_COLOR: Color = Color::srgb(0.8 , 0.8, 0.8);
 
 
-
+#[allow(dead_code)]
 pub fn gen_text_section(
     value: Option<String>,
     size: Option<f32>,
